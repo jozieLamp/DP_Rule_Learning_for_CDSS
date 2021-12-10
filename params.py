313ec9c2e3e8c947a41c2686711b32a2b3303241
jozieLamp <jl4rj@virginia.edu>
2021-12-09 20:30:10 -0500

    Updating coverage runner code

--- params.py
+++ params.py
@@ -13,7 +13,7 @@ paramPercentile = 85 #Percentile wanted for params - score at or below which (in
 
 
 # MCTS PARAMS
-maxQueries = 1000 #Maximum number of server queries allowed (to define for nonprivate model)
+maxQueries = 2000 #Maximum number of server queries allowed (to define for nonprivate model)
 cp = 1/math.sqrt(2) #UCT param to increase or decrease the amount of exploration performed
 maxTreeDepth = 25 #Maximum depth of tree
 cutoffThresh = 0.001 #Match score cutoff threshold to prune branches
