b9bf874f0e586e19d66f060b3ff0f94d5a0b4623
jozieLamp <jl4rj@virginia.edu>
2022-01-21 23:00:55 -0500

    Completing cp assignment in  private model experimentation

--- Run_Experiments_Private.py
+++ Run_Experiments_Private.py
@@ -13,9 +13,6 @@ def main():
     #Main params
     dataset = 'ICU'
     mctsType = 'Base'
-    budgetAllocMethod = 'fixed'
-
-    #TODO - add something to try different private budget strategies ...
 
     #Load rules for experimental purposes
     params.popSize = 10
@@ -23,98 +20,110 @@ def main():
 
     #Load client rules
     clientTrees, clientRules, clientDF = cov.loadClientRules(params.popSize, params.dataFilename)
-    # #Load client data
-    # clientData, clientLabels = RQ.loadClientData(params.popSize, "Data/ICU/DataFrames/")
-    #
-    # #Calculate Rule Quality for client rules
-    # print("First Calculating Client Rule Quality")
-    # clientMCR = RQ.getRulesetMCR(clientRules, clientData, clientLabels)
-    # clientCM = RQ.getSummaryConfusionMatrix(clientData, clientLabels, clientMCR, method='AVG')
-    # clientPtCM = RQ.getPatientConfusionMatrix(clientData, clientLabels, clientMCR, method='AVG')
-    # clientMCR.to_csv("Results/Private/"+ dataset + "/" + mctsType + "_Client_RulesetMCR.csv")
-    # clientCM.to_csv("Results/Private/"+ dataset + "/" + mctsType + "_Client_CM.csv")
-    # clientPtCM.to_csv("Results/Private/" + dataset + "/" + mctsType + "_Client_Patient_CM.csv")
+    #Load client data
+    clientData, clientLabels = RQ.loadClientData(params.popSize, "Data/ICU/DataFrames/")
+
+    #Calculate Rule Quality for client rules
+    print("First Calculating Client Rule Quality")
+    clientMCR = RQ.getRulesetMCR(clientRules, clientData, clientLabels)
+    clientCM = RQ.getSummaryConfusionMatrix(clientData, clientLabels, clientMCR, method='AVG')
+    clientPtCM = RQ.getPatientConfusionMatrix(clientData, clientLabels, clientMCR, method='AVG')
+    clientMCR.to_csv("Results/Private/"+ dataset + "/" + mctsType + "_Client_RulesetMCR.csv")
+    clientCM.to_csv("Results/Private/"+ dataset + "/" + mctsType + "_Client_CM.csv")
+    clientPtCM.to_csv("Results/Private/" + dataset + "/" + mctsType + "_Client_Patient_CM.csv")
 
 
     coverageLst = []
     qualLst = []
     # numQueries = list(range(10000,0,-100))
-    numQueries = [3000, 1000, 500, 100]
-    budgets= [1000, 500, 200, 100, 50, 10]
-
-    for nq in numQueries:
-        for eps in budgets:
-            print("\n\n**************** QUERIES:", nq, "; EPSILON: ", eps, "****************")
-
-            params.maxQueries = nq
-            params.name = mctsType + "_" + str(nq) + "_queries_Eps_" + str(eps)
-            params.resultsFilename = "Results/Private/"+ dataset + "/" + mctsType + "_" + str(nq) + "_queries_Eps" + str(eps)
-
-            #Run protocol
-            runProt(params)
-
-            ## COVERAGE EXPs
-            #Calculate experimental results
-            ldpDF, ldpTrees, ldpRules = cov.loadLDPRuleset(params.resultsFilename + "_Rules.csv")
-
-            print("\nCalculating Coverage")
-            covDF, countDF, clientTrees = cov.getCoverageTable(params.cutoffThresh, ldpDF, ldpTrees, clientDF)
-            print(covDF)
-            structDF = cov.countUniqueStructuresNoVars(clientTrees, ldpTrees)
-            print(structDF)
-
-            #Plot comparison of match counts
-            cov.plotLDPClientCounts(clientDF, countDF, save=params.resultsFilename, title=params.name)
-            #TODO - potentially fix over estimations from multiple partial rules in the rule count part
-
-            lst = [nq, eps, covDF['Total Client Rules'].item(), covDF['Found Rules'].item()/covDF['Total Client Rules'].item() ,covDF['Found Rules'].item(), covDF['Non Rules'].item(),
-                   covDF['Precision'].item(),structDF['Total Client Structures'].item(), structDF['Found Structures'].item()/structDF['Total Client Structures'].item(),
-                   structDF['Found Structures'].item(), structDF['Non Structures'].item(),
-                   structDF['Precision'].item()]
-
-            print("LST", lst)
-            coverageLst.append(lst)
-
-            # ## RULE QUALITY EXPS
-            # # First get misclassification rate of each individual rule
-            # ldpMCR = RQ.getRulesetMCR(ldpRules, clientData, clientLabels)
-            # ldpMCR.to_csv(params.resultsFilename + "_RulesetMCR.csv")
-            # print(ldpMCR)
-            #
-            # # return confusion matrix summary across all patients
-            # ldpCM = RQ.getSummaryConfusionMatrix(clientData, clientLabels, ldpMCR, method='AVG')
-            # ldpCM.to_csv(params.resultsFilename + "_CM.csv")
-            # print(ldpCM)
-            # # return confusion matrix patient by patient
-            # ldpPtCM = RQ.getPatientConfusionMatrix(clientData, clientLabels, ldpMCR, method='AVG')
-            # ldpPtCM.to_csv(params.resultsFilename + "_Patient_CM.csv")
-            # print(ldpPtCM)
-            #
-            # q = [nq, eps, ldpCM['Precision'].item(), ldpCM['Accuracy'].item(), ldpPtCM['Precision'].item(), ldpPtCM['Accuracy'].item()]
-            # qualLst.append(q)
-            # print("Current qual list", qualLst)
+    numQueries = [1000, 500, 100]
+    budgets= [1000, 100, 10, 1, 0.1, 0.01, 0.001]
+    cps = ['epsDivqueries', 'eps', "1"]
 
 
-    #Make final result DFs
-    #Coverage DF
-    df = pd.DataFrame(coverageLst, columns=["Queries", "Epsilon", "Total Client Rules", "Percentage Found Rules", "Found Rules", "Non Rules", "Rule Precision",
-                    "Total Client Structures","Percentage Found Structures", "Found Structures", "Non Structures", "Structure Precision"])
-    df.to_csv("Results/Private/"+ dataset + "/" + mctsType + "_CoverageSummaryDF.csv")
-    print("COVERAGE DF:")
-    print(df)
+    for cpMethod in cps:
+        for nq in numQueries:
+            for eps in budgets:
+                print("\n\n**************** CP: " + cpMethod + "; QUERIES:", nq, "; EPSILON: ", eps, "****************")
 
-    # #Quality DF
-    # qualDF = pd.DataFrame(qualLst, columns=['Queries', "Epsilon", 'Precision', 'Accuracy', 'Patient Precision', 'Patient Accuracy'])
-    # qualDF.to_csv("Results/Private/"+ dataset + "/" + mctsType + "_RuleQualitySummaryDF.csv")
-    # print("RULE QUALITY DF")
-    # print(qualDF)
+                if cpMethod == 'basic':
+                    params.cp = 1 / math.sqrt(2)
+                elif cpMethod == 'epsDivqueries':
+                    params.cp = eps / nq
+                elif cpMethod == 'eps':
+                    params.cp = eps
+                else:
+                    params.cp = 1
 
-    #Make graphs of query analysis for coverage
-    cov.plotQueryAnalysisPrivate(df, save=params.resultsFilename)
+                params.maxQueries = nq
+                params.name = mctsType + "_Cp_" + cpMethod + "_" + str(nq) + "_queries_Eps_" + str(eps)
+                params.resultsFilename = "Results/Private/"+ dataset + "/" + mctsType + "_Cp_" + cpMethod + "_" + str(nq) + "_queries_Eps" + str(eps)
 
-    # # Make graphs of query analysis for rule quality
-    # RQ.plotPrivateCM(qualDF, clientCM, save=params.resultsFilename)
-    # RQ.plotPrivatePatientCM(qualDF, clientPtCM, save=params.resultsFilename)
+                #Run protocol
+                runProt(params)
+
+                ## COVERAGE EXPs
+                #Calculate experimental results
+                ldpDF, ldpTrees, ldpRules = cov.loadLDPRuleset(params.resultsFilename + "_Rules.csv")
+
+                print("\nCalculating Coverage")
+                covDF, countDF, clientTrees = cov.getCoverageTable(params.cutoffThresh, ldpDF, ldpTrees, clientDF)
+                print(covDF)
+                structDF = cov.countUniqueStructuresNoVars(clientTrees, ldpTrees)
+                print(structDF)
+
+                #Plot comparison of match counts
+                cov.plotLDPClientCounts(clientDF, countDF, save=params.resultsFilename, title=params.name)
+                #TODO - potentially fix over estimations from multiple partial rules in the rule count part
+
+                lst = [nq, eps, covDF['Total Client Rules'].item(), covDF['Found Rules'].item()/covDF['Total Client Rules'].item() ,covDF['Found Rules'].item(), covDF['Non Rules'].item(),
+                       covDF['Precision'].item(),structDF['Total Client Structures'].item(), structDF['Found Structures'].item()/structDF['Total Client Structures'].item(),
+                       structDF['Found Structures'].item(), structDF['Non Structures'].item(),
+                       structDF['Precision'].item()]
+
+                print("LST", lst)
+                coverageLst.append(lst)
+
+                ## RULE QUALITY EXPS
+                # First get misclassification rate of each individual rule
+                ldpMCR = RQ.getRulesetMCR(ldpRules, clientData, clientLabels)
+                ldpMCR.to_csv(params.resultsFilename + "_RulesetMCR.csv")
+                print(ldpMCR)
+
+                # return confusion matrix summary across all patients
+                ldpCM = RQ.getSummaryConfusionMatrix(clientData, clientLabels, ldpMCR, method='AVG')
+                ldpCM.to_csv(params.resultsFilename + "_CM.csv")
+                print(ldpCM)
+                # return confusion matrix patient by patient
+                ldpPtCM = RQ.getPatientConfusionMatrix(clientData, clientLabels, ldpMCR, method='AVG')
+                ldpPtCM.to_csv(params.resultsFilename + "_Patient_CM.csv")
+                print(ldpPtCM)
+
+                q = [nq, eps, ldpCM['Precision'].item(), ldpCM['Accuracy'].item(), ldpPtCM['Precision'].item(), ldpPtCM['Accuracy'].item()]
+                qualLst.append(q)
+                print("Current qual list", qualLst)
+
+
+        #Make final result DFs
+        #Coverage DF
+        df = pd.DataFrame(coverageLst, columns=["Queries", "Epsilon", "Total Client Rules", "Percentage Found Rules", "Found Rules", "Non Rules", "Rule Precision",
+                        "Total Client Structures","Percentage Found Structures", "Found Structures", "Non Structures", "Structure Precision"])
+        df.to_csv("Results/Private/"+ dataset + "/" + mctsType + "_CoverageSummaryDF.csv")
+        print("COVERAGE DF:")
+        print(df)
+
+        #Quality DF
+        qualDF = pd.DataFrame(qualLst, columns=['Queries', "Epsilon", 'Precision', 'Accuracy', 'Patient Precision', 'Patient Accuracy'])
+        qualDF.to_csv("Results/Private/"+ dataset + "/" + mctsType + "_RuleQualitySummaryDF.csv")
+        print("RULE QUALITY DF")
+        print(qualDF)
+
+        #Make graphs of query analysis for coverage
+        cov.plotQueryAnalysisPrivate(df, save=params.resultsFilename)
+
+        # Make graphs of query analysis for rule quality
+        RQ.plotPrivateCM(qualDF, clientCM, save=params.resultsFilename)
+        RQ.plotPrivatePatientCM(qualDF, clientPtCM, save=params.resultsFilename)
 
 
 
